3447f77fb8511168bb4c7d45eb45e3426e1feb49
Vivek Kumar Mahto <vkm@Viveks-MacBook-Air.local>
2026-04-21 02:24:16 +0530

    Updated azure web app

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(__dirname));
+// 🔹  MongoDB Connection
+const MONGO_URI =
+  "mongodb+srv://vkm2735:OpenMongoDB123@cluster0.9gfnm.mongodb.net/?appName=Cluster0";
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// 🔹  Schema (same as your users table)
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.post("/add-user", async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    console.log(name, email);
+    const newUser = new User({ name, email });
+    await newUser.save();
+
+    res.send({ message: "User added successfully" });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+// 🔹  API: Get users (same logic)
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/", "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
